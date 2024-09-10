0fedfcb07ded8752e2ad89072e25fcba1e4dfd77
alvin <alvinwongster@gmail.com>
2024-09-10 20:51:09 +0800

    update

--- filehandler.py
+++ filehandler.py
@@ -4,12 +4,16 @@ import random
 
 # Set your paths
 img_dir = 'all_images'
-train_dir = 'train'
-test_dir = 'test'
+train_img_dir = os.path.join('train', 'images')
+train_label_dir = os.path.join('train', 'labels')
+test_img_dir = os.path.join('test', 'images')
+test_label_dir = os.path.join('test', 'labels')
 
-# Ensure train and test directories exist
-os.makedirs(train_dir, exist_ok=True)
-os.makedirs(test_dir, exist_ok=True)
+# Ensure train and test directories and subdirectories exist
+os.makedirs(train_img_dir, exist_ok=True)
+os.makedirs(train_label_dir, exist_ok=True)
+os.makedirs(test_img_dir, exist_ok=True)
+os.makedirs(test_label_dir, exist_ok=True)
 
 # Function to clear a directory
 def clear_directory(directory):
@@ -21,39 +25,36 @@ def clear_directory(directory):
             shutil.rmtree(file_path)
 
 # Clear the train and test directories before starting
-clear_directory(train_dir)
-clear_directory(test_dir)
+clear_directory(train_img_dir)
+clear_directory(train_label_dir)
+clear_directory(test_img_dir)
+clear_directory(test_label_dir)
 
-# Function to copy files to destination
-def copy_files(file_list, dest_dir):
+# Move classes.txt one directory up
+classes_file = os.path.join(img_dir, 'classes.txt')
+if os.path.exists(classes_file):
+    destination_path = os.path.join(os.getcwd(), '..', 'classes.txt')
+    shutil.move(classes_file, destination_path)
+    print(f"Moved classes.txt to {destination_path}")
+else:
+    print("classes.txt does not exist in the 'all_images' folder.")
+
+# Function to copy files to destination (images and labels)
+def copy_files(file_list, img_dest_dir, label_dest_dir):
     for jpg, txt in file_list:
-        shutil.copy(jpg, dest_dir)
-        shutil.copy(txt, dest_dir)
+        shutil.copy(jpg, img_dest_dir)
+        shutil.copy(txt, label_dest_dir)
 
 # List to store file pairs
 file_pairs = []
 
-# Traverse through the files in the current subdirectory
+# Traverse through the files in the img_dir
 for file in os.listdir(img_dir):
     if file.endswith('.jpg'):
         jpg_file = os.path.join(img_dir, file)
         txt_file = os.path.join(img_dir, file.replace('.jpg', '.txt'))
         if os.path.exists(txt_file):
             file_pairs.append((jpg_file, txt_file))
-    
-    file_name = "classes.txt"
-    current_path = os.path.join(os.getcwd(), file_name)
-
-# Check if the file exists and its name matches
-    if os.path.isfile(current_path) and file_name == "classes.txt":
-        # Specify the destination path (one folder up)
-        destination_path = os.path.join(os.getcwd(), "..", file_name)
-        
-        # Move the file
-        shutil.move(current_path, destination_path)
-        print(f"{file_name} has been moved one folder up.")
-    else:
-        print(f"{file_name} does not exist in the current directory.")
 
 # Shuffle the list to ensure randomness
 random.shuffle(file_pairs)
@@ -64,12 +65,12 @@ train_files = file_pairs[:split_index]
 test_files = file_pairs[split_index:]
 
 # Copy train files
-copy_files(train_files, train_dir)
+copy_files(train_files, train_img_dir, train_label_dir)
 
 # Copy test files
-copy_files(test_files, test_dir)
+copy_files(test_files, test_img_dir, test_label_dir)
 
-# Print summary for the current folder
+# Print summary
 total_items = len(file_pairs)
 print(f"Total number of items in {img_dir}: {total_items}")
-print(f"{len(train_files)} items copied to train, {len(test_files)} items copied to test\n")
+print(f"{len(train_files)} items copied to train (images and labels), {len(test_files)} items copied to test (images and labels)")
--- object_detection.py
+++ object_detection.py
@@ -1,46 +1,22 @@
 import os
 import shutil
 import numpy as np
-import pymongo
 
 from ultralytics import YOLO
 from PIL import Image
-from datetime import date 
-
-today = date.today()
-formatted_date = today.strftime("%d-%m-%Y")
-formatted_date_filename = today.strftime("%d_%m_%Y") 
-
-connection_string = "mongodb+srv://alvinwongster:alvinwongster@testcluster.trqdcfv.mongodb.net/?retryWrites=true&w=majority&appName=testcluster"
-try: 
-    client = pymongo.MongoClient(connection_string)
-except Exception as e:
-    print(f"Error: {str(e)}")
-
-database = client["pages"]
-
-#step 4 - create a collection
-myCollection = database["tasks"]
 
 # Load the trained YOLOv8 model
 model = YOLO('best.pt')
 
 # Define the source folder and target folder
-source_folder = "images"
-defects_folder = "defects"
+source_folder = "test_images"
+output_folder = "labelled_images"
 
-# Define custom confidence thresholds for each label (you can add more labels as needed)
-label_thresholds = {
-    'lviv_open': 0.94,  # Threshold for 'label_1'
-    'rgs_open': 0.55,  # Threshold for 'label_2'
-    'rgs_screw_missing': 0.43   # Threshold for 'label_3'
-}
-
-# Check if defects folder exists
-if os.path.exists(defects_folder):
+# Check if the output folder exists
+if os.path.exists(output_folder):
     # If the folder exists, delete all contents
-    for filename in os.listdir(defects_folder):
-        file_path = os.path.join(defects_folder, filename)
+    for filename in os.listdir(output_folder):
+        file_path = os.path.join(output_folder, filename)
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)  # Remove the file
@@ -49,8 +25,8 @@ if os.path.exists(defects_folder):
         except Exception as e:
             print(f'Failed to delete {file_path}. Reason: {e}')
 else:
-    # Create the defects folder if it doesn't exist
-    os.makedirs(defects_folder)
+    # Create the output folder if it doesn't exist
+    os.makedirs(output_folder)
 
 # Loop through the images in the source folder
 for filename in os.listdir(source_folder):
@@ -67,10 +43,6 @@ for filename in os.listdir(source_folder):
         
         # Run YOLO detection
         results = model(img_path)
-
-        # Flag to determine if the image has a valid detection based on the thresholds
-        valid_detection = False
-        detected_label = None
         
         # Check each detection in the results
         for detection in results[0].boxes:
@@ -80,33 +52,15 @@ for filename in os.listdir(source_folder):
             # Get the label name using the model's class names
             label_name = model.names[int(label)]
             
-            # Check if the label is in the threshold dictionary and meets the threshold
-            if label_name in label_thresholds and confidence >= label_thresholds[label_name]:
-                valid_detection = True
-                detected_label = label_name
-                break  # No need to check further if one valid detection is found
-        
-        # If a valid detection is found, copy the image to the defects folder
-        if valid_detection and detected_label:
-            result_img = results[0].plot()  # This returns an image array with the boxes drawn
-            result_pil_image = Image.fromarray(np.uint8(result_img[:,:,[2,1,0]]))
-            
-            # Extract the filename without extension to ensure correct format
-            filename_without_extension = os.path.splitext(filename)[0]
-            new_filename = f"{detected_label}_{filename_without_extension}_{formatted_date_filename}.jpg"
-            output_path = os.path.join(defects_folder, new_filename)
-            result_pil_image.save(output_path)  # Save the plotted image
+            # This returns an image array with the boxes drawn
+            result_img = results[0].plot()
 
-            task_data = {
-                "date": formatted_date,  # Date with dashes for the database
-                "location": "side",
-                "sensor": "sony",
-                "task_status": False,  # Default status is incomplete
-                "image_name": new_filename,
-            }
-            try:
-                myCollection.insert_one(task_data)
-                print(f"Data for {new_filename} saved to MongoDB.")
-            except Exception as e:
-                print(f"Failed to save data for {new_filename}. Error: {e}")
+            # Convert array back to an image (ensure the correct color channel order)
+            result_pil_image = Image.fromarray(np.uint8(result_img))
+
+            # Save the image to the output folder with the original filename
+            result_pil_image.save(os.path.join(output_folder, filename))
+
+        print(f"Processed and saved {filename} with YOLO detection results.")
+
 
